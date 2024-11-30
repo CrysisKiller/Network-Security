4a7eb78359c42aaa2c40a67fd88b4124fc41fb91
CrysisKiller <sudhamshu.h1@gmail.com>
2024-11-30 18:21:27 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ def get_requirements(file_path:str)->List[str]:
     this function will return the list of requirements
     '''
     requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+    try:
+        with open(file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
     
-    return requirements
+        return requirements
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
 
 setup(
 name='mlproject',
